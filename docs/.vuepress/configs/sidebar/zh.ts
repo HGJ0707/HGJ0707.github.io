812315ae7e9a3ef96eba937dddab7d14269e397e
guangjun <huguangjun@xxpie.com>
2023-06-23 23:02:38 +0800

    #feat: js新增闭包

--- docs/.vuepress/configs/sidebar/zh.ts
+++ docs/.vuepress/configs/sidebar/zh.ts
@@ -9,6 +9,7 @@ export const sidebarZh: SidebarConfig = {
         '/zh/javascript/1-变量声明.md', 
         '/zh/javascript/2-数据类型.md', 
         '/zh/javascript/3-this.md', 
+        '/zh/javascript/4-闭包.md', 
       ],
     },
     // {
